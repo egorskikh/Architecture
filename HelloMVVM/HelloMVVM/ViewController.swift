54764a15da31759a29147b94d74fd96a7d8433cd
Egor Gorskikh <egorgorskikh@gmail.com>
2021-08-10 17:54:35 +0300

    init: MVVM

---
+++ HelloMVVM/HelloMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMVVM
+//
+//  Created by Egor Gorskikh on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
+
